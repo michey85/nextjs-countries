a24ca915bbf1d568a7c801bc98b7f11244d69752
Mikhail Nepomniashchii <michey85@gmail.com>
2024-09-28 14:27:00 +0200

    add ramda

--- src/services/getAllCountries.ts
+++ src/services/getAllCountries.ts
@@ -1,6 +1,10 @@
+import { Country } from '@/types/Country';
+
 const API_URL = 'https://restcountries.com/v3.1/all';
 
-export default async function getAllCountries() {
+export default async function getAllCountries(): Promise<Country[]> {
   const res = await fetch(API_URL);
-  return res.json();
+  const data = (await res.json()) as Country[];
+
+  return data;
 }
