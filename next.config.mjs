4ecdf75cebab05088f8622d77d19507adb7d6976
Mikhail Nepomniashchii <michey85@gmail.com>
2024-09-28 14:19:02 +0200

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'flagcdn.com', 'upload.wikimedia.org'],
+  },
+};
 
 export default nextConfig;
